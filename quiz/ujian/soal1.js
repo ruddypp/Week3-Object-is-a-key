ff8e6c96a88a652f3138698c03b574ca2a3921ef
Rudy Paningal <paningalrudy@gmail.com>
2025-03-10 01:25:42 +0700

    ujian

--- quiz/ujian/soal1.js
+++ quiz/ujian/soal1.js
@@ -1,6 +1,16 @@
 function deepSum (arr) {
     // Code disini
+
+    let sum = 0 ;
+    for (let i = 0; i < arr.length; i++) {
+      for(let j = 0; j < arr[i].length; j++) {
+        for(let k = 0; k < arr[i][j].length; k++) {
+          sum += arr[i][j][k];
+      }
+    } 
   }
+  return sum === 0 ? "No Number" : sum;
+}
   
   //TEST CASE
   console.log(deepSum([
--- quiz/ujian/soal2.js
+++ quiz/ujian/soal2.js
@@ -1,7 +1,28 @@
 function naikAngkot(arrPenumpang) {
     rute = ['A', 'B', 'C', 'D', 'E', 'F'];
     //your code here
+
+    let hasil = [];
+    for (let i = 0; i < arrPenumpang.length; i++) {
+        let penumpang = arrPenumpang[i][0];
+        let naikDari = arrPenumpang [i][1];
+        let tujuan = arrPenumpang [i][2];
+
+        let indexNaik = rute.indexOf(naikDari);
+        let indexTujuan = rute.indexOf(tujuan);
+        let bayar = (indexTujuan - indexNaik) * 2000;
+      
+        hasil.push({ 
+          penumpang : penumpang, 
+          naikDari : naikDari, 
+          tujuan : tujuan, 
+          bayar : bayar
+         });
+
+         return hasil;
+  
   }
+}
   
   //TEST CASE
   console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
